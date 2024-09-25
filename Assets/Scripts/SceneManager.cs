13a4e71cf2644464cf14de4b99bd6a35235ebc7a
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2024-09-25 11:19:39 +0200

    update

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,21 +1,28 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
     public GameObject menuOverlay;
-    public GameObject play;
-    public GameObject credits;
-    public GameObject exit;
+    public Button play;
+    public Button credits;
+    public Button exit;
 
 
     void Start()
     {
+        FindObjectOfType<ObstacleCollision>().OnGameOver += OnGameOver;
+    }
+
+    private void Update()
+    {
         
     }
 
-
-    void Update()
+    void OnGameOver()
     {
-        
+        menuOverlay.SetActive(true);
     }
 }
