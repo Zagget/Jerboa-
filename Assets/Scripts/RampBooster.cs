bd120bc74cc30c73f6f25be634649bb6f90a91f8
leopeoprogamer <leolundberg100@gmail.com>
2024-09-25 13:37:51 +0200

    Ramp Booster And Flip Portal

---
+++ Assets/Scripts/RampBooster.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampBooster : MonoBehaviour
+{
+    public float maxBoostSpeed = 10f;
+    public float acceleration = 5f;
+    Vector2 boostDirection = Vector2.right;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+
+            if (playerRb != null)
+            {
+                playerController.enabled = false;
+
+                float currentSpeed = Vector2.Dot(playerRb.velocity, boostDirection.normalized);
+
+                if (currentSpeed < maxBoostSpeed)
+                {
+                    Vector2 additionalVelocity = boostDirection.normalized * acceleration * Time.fixedDeltaTime;
+
+                    playerRb.velocity += additionalVelocity;
+
+                    if (Vector2.Dot(playerRb.velocity, boostDirection.normalized) > maxBoostSpeed)
+                    {
+                        playerRb.velocity = boostDirection.normalized * maxBoostSpeed;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+
+            playerController.enabled = true;
+        }
+    }
+
+}
+
