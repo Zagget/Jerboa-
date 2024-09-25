4a51a2e0e8bae85bff75150650adfa015239b834
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2024-09-25 10:36:40 +0200

    Added menu(in progress) and cleaned up hierarchy

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject menuOverlay;
+    public GameObject play;
+    public GameObject credits;
+    public GameObject exit;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
